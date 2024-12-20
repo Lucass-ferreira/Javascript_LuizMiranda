6071abffc23701b1352bd0ec4f10d00a192197ea
Lucas Ferreira <lucasferreiragxhxhs@gmail.com>
2024-12-20 15:34:38 -0300

    ex switch

--- Logica_introdução_js/aula30/ex_switch/assets/js/main.js
+++ Logica_introdução_js/aula30/ex_switch/assets/js/main.js
@@ -1,39 +1,67 @@
+const h1 = document.querySelector(".container h1")
+const data = new Date()
+// console.log(data.getDay()) // dia semana
+// console.log(data.getDate()) // data
+// console.log(data.getMonth()) // mês
+// console.log(data.getFullYear()) // ano
+// console.log(data.getHours()) // hora
+// console.log(data.getMinutes()) // minutos
 
-const h1 = document.querySelector('.container h1');
-const data = new Date();
-
-function getDiaSemanaTexto(diaSemana) {
-  const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 
-    'sexta', 'sábado'];
-  return diasSemana[diaSemana];
+function diaSemana(num_dia) {
+    let diaSemana;
+    switch (num_dia) {
+        case 0:
+            diaSemana = 'Domingo'
+            break;
+        case 1:
+            diaSemana = 'Segunda-feira'
+            break;
+        case 2:
+            diaSemana = 'Terça-feira'
+            break;
+        case 3:
+            diaSemana = 'Quarta-feira'
+            break;
+        case 4:
+            diaSemana = 'Quinta-feira'
+            break;
+        case 5:
+            diaSemana = 'Sexta-feira'
+            break;
+        case 6:
+            diaSemana = 'Sabado'
+            break;
+        default:
+            diaSemana = " "
+            break;
+    }
+    return diaSemana
 }
 
-function getNomeMes(numeroMes) {
-  const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maior', 'junho', 
-    'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
-  return meses[numeroMes];
+function mesDoAno(num_mes){
+    const arrMes = [
+        "janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro"
+    ]
+
+    if(num_mes > 0 && num_mes < 13){
+        return arrMes[num_mes]
+    }
 }
 
-function zeroAEsquerda (num) {
-  return num >= 10 ? num : `0${num}`;
-}
+const dia_semana = diaSemana(data.getDay())
+const Mes_ano = mesDoAno(data.getMonth())
+// console.log(h1.textContent)
 
-function criaData(data) {
-  const diaSemana = data.getDay();
-  const numeroMes = data.getMonth();
-
-  const nomeDia = getDiaSemanaTexto(diaSemana);
-  const nomeMes = getNomeMes(numeroMes);
-
-  return (
-    `${nomeDia}, ${data.getDate()} de ${nomeMes}` + 
-    ` de ${data.getFullYear()} ` + 
-    `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
-  );
-}
-
-h1.innerHTML = criaData(data);
-
-const h1 = document.querySelector('.container h1');
-const data = new Date();
-h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short' });
+//segunda-feira, 20 de Dezembro de 2024, 15:30
+h1.textContent = `${dia_semana}, ${data.getDate()} de ${Mes_ano} de ${data.getFullYear()}, ${data.getHours()}:${data.getMinutes()}`
