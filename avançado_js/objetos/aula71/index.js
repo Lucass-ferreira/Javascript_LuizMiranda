192c150664966897da740b1986166afb5cfa5806
Lucas Ferreira <lucasferreiragxhxhs@gmail.com>
2025-01-23 10:44:17 -0300

    defineProperty - defineProperties

---
+++ avançado_js/objetos/aula71/index.js
@@ -0,0 +1,38 @@
+// defineProperty - defineProperties
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+  
+    // para uma chave
+    Object.defineProperty(this, 'estoque', {
+      enumerable: true, // mostra a chave
+      value: estoque, // valor
+      writable: true, // pode alterar
+      configurable: true // configurável
+    });
+
+    // para varias chaves
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
+        },
+    })
+  }
+  
+  const p1 = new Produto('Camiseta', 20, 3);
+//   console.log(Object.keys(p1)); // mostra as chaves do obejeto
+  console.log((p1));
+  
+  for(let chave in p1) {
+    console.log(chave);
+  }
